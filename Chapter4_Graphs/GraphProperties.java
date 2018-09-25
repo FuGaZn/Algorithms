a8fbb0003d13f43a6481231bf2d82bbf9f1cc1ad
FuGaZn <15298605109@163.com>
2018-09-25 21:31:45 +0800

    no message

--- Chapter4_Graphs/GraphProperties.java
+++ Chapter4_Graphs/GraphProperties.java
@@ -1,85 +1,5 @@
-4.1.1
+package Code;
 
-至多2^(V-2)+V条边
-
-连通图，至少V-1条边
-
-<br>
-
-4.1.3
-
-```java
-public Graph(Graph G){
-  this.V = G.V();
-  this.E = G.E();
-  adj = (Bag<Integer>[]) new Bag[G.V()];
-  for(int v= 0;v<V;v++)
-    adj[v] = new Bag(G.adj[v]);
-}
-```
-
-
-
-4.1.4
-
-```java
-public boolean hasEdge(int v, int w){
-  for(int i: this.adj[v]){
-    if(i == w)
-      return true;
-  }
-  return false;
-}
-```
-
-
-
-4.1.5
-
-```java
-public void addEdge(int v, int w){
-  if(!this.hasEdge(v, w) && v != w){
-    this.adj[v].add(w);
-    this.adj[w].add(v);
-    E++;
-  }
-}
-```
-
-
-
-4.1.12
-
-```java
-public class BFS{
-	public int bfs(Graph G, int v, int w) {
-        int distance = 0;
-        int size = 0;
-        Queue<Integer> queue = new Queue<Integer>();
-        queue.enqueue(v);
-        size++;
-        while (!queue.isEmpty()) {
-            int tmp = size;
-            size = 0;
-            while (tmp > 0) {
-                int q = queue.dequeue();
-                for (int i : G.adj(q))
-                    if (i != w) {
-                        queue.enqueue(i);
-                        size++;
-                    } else return distance;
-                tmp--;
-            }
-            distance++;
-        }
-        return distance;
-    }
-}
-```
-
-4.1.16/17
-
-```java
 import edu.princeton.cs.algs4.*;
 
 public class GraphProperties {
@@ -91,6 +11,7 @@ public class GraphProperties {
     private int radius = 0;
     private int center = 0;
     private int girth = 0;
+
 
     GraphProperties(Graph G) {
         pathlen = new int[G.V()];
@@ -154,6 +75,7 @@ public class GraphProperties {
         return girth == 0 ? Integer.MAX_VALUE : girth;
     }
 
+
     private void bfs(Graph g, int v) {
         for (int i = 0; i < pathlen.length; i++) {
             pathlen[i] = 0;
@@ -180,11 +102,3 @@ public class GraphProperties {
         }
     }
 }
-
-```
-
-
-
-4.1.28
-
-修改cycle，允许存在自环和平行边。
